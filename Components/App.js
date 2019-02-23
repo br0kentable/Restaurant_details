d4c8846bb6a8c41f13b1055b39a2d64de2468807
Michael Ryan <michaeltheryan@gmail.com>
2019-02-23 12:19:25 -0500

    Filled in webpack config file, setup initial react components, sketched in the API call to the database, and made additional small code refinements

--- Components/App.js
+++ Components/App.js
@@ -1,3 +1,38 @@
-﻿import { hot } from 'react-hot-loader/root'
-const App = () => <div>Hello World!</div>
+﻿import React, {Component} from "react";
+import { hot } from 'react-hot-loader/root';
+const axios = require('axios');
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {name: 'Unknown'};
+
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('/listings?id=thfUjkN4Tv')
+      .then(function (response) {
+        this.setState({
+          name: res.name
+        })
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      // .then(function () {
+      // });
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  }
+}
+
 export default hot(App)
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@
 
 module.exports = {
   entry: './src/index.js',
-  output: {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loaders: ["react-hot"],
+        include: path.join(__dirname, "public")
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  // plugins: [
-  //   new webpack.HotModuleReplacementPlugin()
-  // ],
-  // rules: [
-  //   {
-  //     test: /\.jsx?$/,
-  //     loaders: [‘react-hot’],
-  //     include: path.join(__dirname, ‘public’)
-  // },
-  // {
-  //     test: /\.(js|jsx)$/,
-  //     exclude: /node_modules/,
-  //     use: {
-  //       loader: "babel-loader"
-  //     }
-  //   },
-  // {
-  //   test: /\.jsx?$/,
-  //   include: /node_modules/,
-  //   use: ['react-hot-loader/webpack'],
-  // },
-  // ]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 };
